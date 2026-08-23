ed907f25ff0aadb54df37a9e52451d5e5cbc246b
AdEonnnnn <aryamany586@gmail.com>
2026-08-23 16:42:16 +0530

    Day 15

---
+++ day 15/30.c
@@ -0,0 +1,22 @@
+/*
+Write a program to reverse a given number.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int num, reverse = 0, rem;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while (num != 0) {
+        rem = num % 10;
+        reverse = reverse * 10 + rem;
+        num /= 10;
+    }
+
+    printf("Reversed number = %d\n", reverse);
+
+    return 0;
+}
